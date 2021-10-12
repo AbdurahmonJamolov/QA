7fd98ef7714172a292bdede49a954fcd06334768
AbdurahmonJamolov <75623049+AbdurahmonJamolov@users.noreply.github.com>
2021-10-12 15:40:13 +0300

    Add tests #3

--- src/test/java/SkippListTest.java
+++ src/test/java/SkippListTest.java
@@ -13,6 +13,28 @@ public class SkippListTest {
         Assert.assertTrue(list.delete(2));
     }
     
+     @Test
+    public void deleteMissing(){
+        SkippableList<Integer> list = new SkipList<>();
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        
+        Assert.assertFalse(list.delete(10));
+    }
+    
+     @Test
+     public void deleteOneOfMany(){
+         SkippableList<Integer> list = new SkipList<>();
+         list.insert(1);
+         list.insert(2);
+         list.insert(2);
+         list.insert(3);
+         list.insert(4);
+         Assert.assertTrue(list.delete(2));
+     }
+    
     @Test
     public void search(){
         SkippableList<Integer> list = new SkipList<>();
